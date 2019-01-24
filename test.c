f4d02f8e93f84415cae3bd91acd3f9b0a8b325b5
joseph frampton <josephjamesframpton@gmail.com>
2019-01-24 04:59:50 -0700

    added renamed files - will now have to copy .txt to .c then compile
    JJF

---
+++ test.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/*
+*linked_list_implementation*
+WIP
+*/
+
+typedef struct Nodes {
+  char value[50];
+  // Node *next;
+  int link;
+} Node;
+
+typedef struct LinkedLists {
+  Node head;
+  Node node;
+  insert_node
+} LinkedList;
+
+int main() {
+  printf("\nstart program\n");
+  Node node;
+  strcpy(node.value, "one");
+  node.link = 12;
+}
